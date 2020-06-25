5dc8718501f173d2a1eee9356015783b7cb56b2b
Felipe Torrezan <felipe.torrezan@iar.com>
2020-06-25 16:55:13 +0200

    Added template project

---
+++ Examples/template/README.md
@@ -0,0 +1,5 @@
+# Clock tree setup example
+
+This example only provides the clock tree setup for the GD32V103F device. 
+  
+System clock is set to 108 MHz.
---
+++ Examples/template/main.c
@@ -0,0 +1,30 @@
+/*!
+    \file  main.c
+    \brief Template for starting new projects
+    
+    \version 20200625
+*/
+
+/*
+    Copyright (c) 2020, IAR Systems AB.
+
+    See LICENSE.md for detailed license information.
+*/
+
+#include "iar-risc-v-gd32v-eval.h"
+
+/*!
+    \brief      main function
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
+void main(void)
+{
+    SystemInit();
+    
+    while(1)
+    {
+
+    }
+}
