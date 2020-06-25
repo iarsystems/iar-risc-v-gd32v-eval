256e68f4578208ce1a64dd346e1edbd085030f76
Felipe Torrezan <felipe.torrezan@iar.com>
2020-06-25 16:52:16 +0200

    Updated USART Example

--- Examples/usart/gd32vf103_it.h
+++ Examples/usart/gd32vf103_it.h
@@ -1,35 +1,14 @@
 /*!
     \file  gd32vf103_it.h
-    \brief the header file of the ISR
+    \brief Header file for the interrupt service routines
     
-    \version 2020-05-21, V1.0.0, GD32VF103 firmware for IAR
+    \version 20200625
 */
 
 /*
-    Copyright (c) 2020, GigaDevice Semiconductor Inc.
+    Copyright (c) 2020, IAR Systems AB.
 
-    Redistribution and use in source and binary forms, with or without modification, 
-are permitted provided that the following conditions are met:
-
-    1. Redistributions of source code must retain the above copyright notice, this 
-       list of conditions and the following disclaimer.
-    2. Redistributions in binary form must reproduce the above copyright notice, 
-       this list of conditions and the following disclaimer in the documentation 
-       and/or other materials provided with the distribution.
-    3. Neither the name of the copyright holder nor the names of its contributors 
-       may be used to endorse or promote products derived from this software without 
-       specific prior written permission.
-
-    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
-IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
-INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
-NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
-PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
-WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
-OF SUCH DAMAGE.
+    See LICENSE.md for detailed license information.
 */
 
 #ifndef GD32VF103_IT_H
@@ -38,8 +17,8 @@ OF SUCH DAMAGE.
 #include "gd32vf103.h"
 
 /* function declarations */
-/* TIMER1 handle function */
-void TIMER1_IRQHandler(void);
-void ADC0_1_IRQHandler(void);
+/* handles the USART1 interrupt */
+void USART1_IRQHandler(void);
+
 
 #endif /* GD32VF103_IT_H */
--- Examples/usart/main.c
+++ Examples/usart/main.c
@@ -2,45 +2,23 @@
     \file  main.c
     \brief USART transmit and receive interrupt
 
-    \version 2019-06-05, V1.0.0, firmware for GD32VF103
+    \version 20200625 
 */
 
 /*
-    Copyright (c) 2019, GigaDevice Semiconductor Inc.
+    Copyright (c) 2020, IAR Systems AB.
 
-    Redistribution and use in source and binary forms, with or without modification, 
-are permitted provided that the following conditions are met:
-
-    1. Redistributions of source code must retain the above copyright notice, this 
-       list of conditions and the following disclaimer.
-    2. Redistributions in binary form must reproduce the above copyright notice, 
-       this list of conditions and the following disclaimer in the documentation 
-       and/or other materials provided with the distribution.
-    3. Neither the name of the copyright holder nor the names of its contributors 
-       may be used to endorse or promote products derived from this software without 
-       specific prior written permission.
-
-    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
-IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
-INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
-NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
-PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
-WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
-OF SUCH DAMAGE.
+    See LICENSE.md for detailed license information.
 */
 
-#include "gd32vf103.h"
 #include <stdio.h>
 
-/* configure the UART peripheral */
-void com_init(uint32_t com);
+#include "iar-risc-v-gd32v-eval.h"
 
-#define ARRAYNUM(arr_nanme)      (uint32_t)(sizeof(arr_nanme) / sizeof(*(arr_nanme)))
-#define TRANSMIT_SIZE            (ARRAYNUM(txbuffer) - 1)
+#define ARRAYNUM(arr_name)      (uint32_t)(sizeof(arr_name) / sizeof(*(arr_name)))
+#define TRANSMIT_SIZE           (ARRAYNUM(txbuffer) - 1)
 
+/* Global variables */
 uint8_t txbuffer[] = "\n\rUSART interrupt test\n\r";
 uint8_t rxbuffer[32];
 uint8_t tx_size = TRANSMIT_SIZE;
@@ -54,66 +32,37 @@ __IO uint16_t rxcount = 0;
     \param[out] none
     \retval     none
 */
-int main(void)
+void main(void)
 {
     SystemInit();
-    /* USART interrupt configuration */
+    
+    /* EVAL_COM1 interrupt configuration */
     eclic_global_interrupt_enable();
     eclic_priority_group_set(ECLIC_PRIGROUP_LEVEL3_PRIO1);
     eclic_irq_enable(USART1_IRQn, 1, 0);
     
-    /* configure USART1*/
-    com_init(USART1);
-    /* enable USART TBE interrupt */  
-    usart_interrupt_enable(USART1, USART_INT_TBE);
+    /* configure IAR RISC-V GD32V EVAL COM1*/
+    gd_eval_com_init(EVAL_COM1);
     
-    /* wait until USART send the transmitter_buffer */
-    while(txcount < tx_size);
+    /* enable EVAL_COM1 TBE interrupt */  
+    usart_interrupt_enable(EVAL_COM1, USART_INT_TBE);
     
-    while(RESET == usart_flag_get(USART1, USART_FLAG_TC));
+    /* wait until the transmit buffer is sent to EVAL_COM1 */
+    while (txcount < tx_size);
     
-    usart_interrupt_enable(USART1, USART_INT_RBNE);
+    while (RESET == usart_flag_get(EVAL_COM1, USART_FLAG_TC));
     
-    /* wait until USART receive the receiver_buffer */
-    while(rxcount < rx_size);
-    if(rxcount == rx_size)
-        printf("\n\rUSART receive successfully!\n\r");
+    usart_interrupt_enable(EVAL_COM1, USART_INT_RBNE);
+    
+    /* wait until EVAL_COM1 receives the receive buffer */
+    while (rxcount < rx_size);
+    
+    if (rxcount == rx_size)
+    {
+        /* Prints on the Terminal I/O */
+        /* Enable it on View -> Terminal I/O */
+        printf("\n\rUSART data received successfully!\n\r");
+    }
+    
     while(1);
 }
-
-
-/*!
-    \brief      configure COM port
-    \param[in]  com: COM on the board
-      \arg        EVAL_COM0: COM0 on the board
-      \arg        EVAL_COM1: COM1 on the board
-    \param[out] none
-    \retval     none
-*/
-void com_init(uint32_t com)
-{
-    /* enable GPIO clock */
-    rcu_periph_clock_enable(RCU_GPIOA);
-
-    /* enable USART clock */
-    rcu_periph_clock_enable(RCU_USART1);
-
-    /* connect port to USARTx_Tx */
-    gpio_init(GPIOA, GPIO_MODE_AF_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_2);
-
-    /* connect port to USARTx_Rx */
-    gpio_init(GPIOA, GPIO_MODE_IN_FLOATING, GPIO_OSPEED_50MHZ, GPIO_PIN_3);
-
-    /* USART configure */
-    usart_deinit(USART1);
-    usart_baudrate_set(USART1, 115200U);
-    usart_word_length_set(USART1, USART_WL_8BIT);
-    usart_stop_bit_set(USART1, USART_STB_1BIT);
-    usart_parity_config(USART1, USART_PM_NONE);
-    usart_hardware_flow_rts_config(USART1, USART_RTS_DISABLE);
-    usart_hardware_flow_cts_config(USART1, USART_CTS_DISABLE);
-    usart_receive_config(USART1, USART_RECEIVE_ENABLE);
-    usart_transmit_config(USART1, USART_TRANSMIT_ENABLE);
-    usart_enable(USART1);
-}
-
