9142028c6f2ad14de01f18fe067c2c6fb8878203
Felipe Torrezan <felipe.torrezan@iar.com>
2020-08-10 10:03:46 +0200

    Updated BSP I2C wrapper
    - initialized i variable on bsp_i2c0_read()
    - prefixed all public functions with bsp_
    - added initial I2C status handling
    - updated related project examples and drivers

--- bsp/MMA8652.c
+++ bsp/MMA8652.c
@@ -1,6 +1,6 @@
 /*!
 \file    MMA8652.c
-\brief   MMA8652 3-axis accelerometer basic functions 
+\brief   MMA8652 3-axis accelerometer basic functions
 
 \version 20200708
 */
@@ -25,29 +25,29 @@ static void mma8652_calibration(void)
     int16_t  x_out,    y_out,    z_out;
     int8_t   x_offset, y_offset, z_offset;
     uint8_t  mma8652_rxbuffer[7];
-    
+
     // 0x00 to MMA8652_CTRL_REG1 to place MMA8652 into standby.
-    i2c_reg8_write(MMA8652_I2C_ADDR, MMA8652_CTRL_REG1, 0x00);
-    
-    i2c0_read((uint8_t)MMA8652_I2C_ADDR, (uint8_t *) mma8652_rxbuffer, sizeof(mma8652_rxbuffer));
-    
+    bsp_i2c0_reg8_write(MMA8652_I2C_ADDR, MMA8652_CTRL_REG1, 0x00);
+
+    bsp_i2c0_read((uint8_t)MMA8652_I2C_ADDR, (uint8_t *) mma8652_rxbuffer, sizeof(mma8652_rxbuffer));
+
     /* 12-bit X-,Y-,Z-axis acceleration output value */
-    x_out = ((int16_t)(mma8652_rxbuffer[1] << 8 | mma8652_rxbuffer[2])) >> 4;  
-    y_out = ((int16_t)(mma8652_rxbuffer[3] << 8 | mma8652_rxbuffer[4])) >> 4;  
-    z_out = ((int16_t)(mma8652_rxbuffer[5] << 8 | mma8652_rxbuffer[6])) >> 4;  
-    
+    x_out = ((int16_t)(mma8652_rxbuffer[1] << 8 | mma8652_rxbuffer[2])) >> 4;
+    y_out = ((int16_t)(mma8652_rxbuffer[3] << 8 | mma8652_rxbuffer[4])) >> 4;
+    z_out = ((int16_t)(mma8652_rxbuffer[5] << 8 | mma8652_rxbuffer[6])) >> 4;
+
     /* compute the offset corrections */
-  	x_offset = x_out / 2 * (-1);		
-  	y_offset = y_out / 2 * (-1);		
-  	z_offset = (z_out - MMA8652_SENSITIVITY_2G) / 2 * (-1);		
-    
+  	x_offset = x_out / 2 * (-1);
+  	y_offset = y_out / 2 * (-1);
+  	z_offset = (z_out - MMA8652_SENSITIVITY_2G) / 2 * (-1);
+
     /* apply the offset corrections */
-    i2c_reg8_write(MMA8652_I2C_ADDR, MMA8652_OFF_X_REG, x_offset);
-    i2c_reg8_write(MMA8652_I2C_ADDR, MMA8652_OFF_Y_REG, y_offset);
-    i2c_reg8_write(MMA8652_I2C_ADDR, MMA8652_OFF_Z_REG, z_offset);
-    
+    bsp_i2c0_reg8_write(MMA8652_I2C_ADDR, MMA8652_OFF_X_REG, x_offset);
+    bsp_i2c0_reg8_write(MMA8652_I2C_ADDR, MMA8652_OFF_Y_REG, y_offset);
+    bsp_i2c0_reg8_write(MMA8652_I2C_ADDR, MMA8652_OFF_Z_REG, z_offset);
+
     /* 0x11 to MMA8652_CTRL_REG1 activate */
-    i2c_reg8_write(MMA8652_I2C_ADDR, MMA8652_CTRL_REG1, 0x11);
+    bsp_i2c0_reg8_write(MMA8652_I2C_ADDR, MMA8652_CTRL_REG1, 0x11);
 }
 
 /*!
@@ -59,11 +59,11 @@ static void mma8652_calibration(void)
 void mma8652_init(void)
 {
     /* 0x00 to MMA8652_CTRL_REG1 to place MMA8652 into standby */
-    i2c_reg8_write(MMA8652_I2C_ADDR, MMA8652_CTRL_REG1, 0x00);
-    
+    bsp_i2c0_reg8_write(MMA8652_I2C_ADDR, MMA8652_CTRL_REG1, 0x00);
+
     /* high-resolution mode */
-    i2c_reg8_write(MMA8652_I2C_ADDR, MMA8652_CTRL_REG2, 0x02);
-    
+    bsp_i2c0_reg8_write(MMA8652_I2C_ADDR, MMA8652_CTRL_REG2, 0x02);
+
     /*
     * Write 0000 0001 = 0x01 to XYZ_DATA_CFG register to set g range
     * [7-5]: reserved=000
@@ -71,8 +71,8 @@ void mma8652_init(void)
     * [3-2]: reserved=00
     * [1-0]: FS=01 for +/-4g: 512 counts / g = 8192 counts / g after 4 bit left shift
     */
-    i2c_reg8_write(MMA8652_I2C_ADDR, MMA8652_XYZ_DATA_CFG, 0x01);
-    
+    bsp_i2c0_reg8_write(MMA8652_I2C_ADDR, MMA8652_XYZ_DATA_CFG, 0x01);
+
     /*
     * write 0000 0010 = 0x02 to MMA8652_CTRL_REG2 to set MODS bits
     * [7]: ST=0: self test disabled
@@ -82,8 +82,8 @@ void mma8652_init(void)
     * [2]: SLPE=0: auto sleep disabled
     * [1-0]: mods=10 for high resolution (maximum over sampling)
     */
-    i2c_reg8_write(MMA8652_I2C_ADDR, MMA8652_CTRL_REG2, 0x02);
-    
+    bsp_i2c0_reg8_write(MMA8652_I2C_ADDR, MMA8652_CTRL_REG2, 0x02);
+
     /*
     * write 0001 0001 = 0x11 to MMA8652_CTRL_REG1
     * [7-6]: aslp_rate=00
@@ -92,8 +92,8 @@ void mma8652_init(void)
     * [1]: f_read=0 for normal 16 bit reads
     * [0]: active=1 to take the part out of standby and enable sampling
     */
-    i2c_reg8_write(MMA8652_I2C_ADDR, MMA8652_CTRL_REG1, 0x11);
-    
+    bsp_i2c0_reg8_write(MMA8652_I2C_ADDR, MMA8652_CTRL_REG1, 0x11);
+
     /* performs the MMA8652 calibration */
     mma8652_calibration();
 }
@@ -110,16 +110,16 @@ void mma8652_getxyz(float *x, float *y, float *z)
 {
     uint8_t mma8652_rxbuffer[7];
     int16_t x_out, y_out, z_out;
-    
-    i2c0_read((uint8_t)MMA8652_I2C_ADDR, (uint8_t *) mma8652_rxbuffer, sizeof(mma8652_rxbuffer));
-    
+
+    bsp_i2c0_read((uint8_t)MMA8652_I2C_ADDR, (uint8_t *) mma8652_rxbuffer, sizeof(mma8652_rxbuffer));
+
     /* 12-bit (X-,Y-,Z-axis acceleration output value */
     x_out = ((int16_t)(mma8652_rxbuffer[1] << 8 | mma8652_rxbuffer[2])) >> 4;
     y_out = ((int16_t)(mma8652_rxbuffer[3] << 8 | mma8652_rxbuffer[4])) >> 4;
     z_out = ((int16_t)(mma8652_rxbuffer[5] << 8 | mma8652_rxbuffer[6])) >> 4;
-    
+
     /* X-,Y-,Z-axis output value (in g's) */
-    *x = ((float) x_out) / MMA8652_SENSITIVITY; 
-    *y = ((float) y_out) / MMA8652_SENSITIVITY; 
-    *z = ((float) z_out) / MMA8652_SENSITIVITY; 
+    *x = ((float) x_out) / MMA8652_SENSITIVITY;
+    *y = ((float) y_out) / MMA8652_SENSITIVITY;
+    *z = ((float) z_out) / MMA8652_SENSITIVITY;
 }
--- bsp/i2c.c
+++ bsp/i2c.c
@@ -1,8 +1,8 @@
 /*!
-\file    i2c.c
-\brief   BSP I2C functions
+    \file    i2c.c
+    \brief   BSP I2C functions
 
-\version 20200708
+    \version 20200810
 */
 
 /*
@@ -14,36 +14,33 @@ See LICENSE.md for detailed license information.
 #include "i2c.h"
 
 /*!
-    \brief      write to a i2c slave 
+    \brief      write to a I2C slave
     \param[in]  i2c_dev_7bit_addr: the slave's 7-bit address
     \param[in]  data: pointer to the 8-bit variable containing the value
     \param[in]  count: number of bytes to write
     \param[out] none
     \retval     status
 */
-uint8_t i2c0_write(uint8_t i2c_dev_7bit_addr, uint8_t *data, uint8_t count)
+i2c_state_t bsp_i2c0_write(uint8_t i2c_dev_7bit_addr, uint8_t *data, uint8_t count)
 {
-    if (count < 1) return -1; // reserved for status handling
-    
+    if (count < 1) return BSP_I2C_FAIL;
+
     uint8_t i = count;
-    
+
     /* wait until I2C bus is idle */
     while (i2c_flag_get(I2C0, I2C_FLAG_I2CBSY));
     /* send a start condition to I2C bus */
     i2c_start_on_bus(I2C0);
     /* wait until SBSEND bit is set */
     while (!i2c_flag_get(I2C0, I2C_FLAG_SBSEND));
-    
     /* send slave address to I2C bus */
     i2c_master_addressing(I2C0, (i2c_dev_7bit_addr << 1), I2C_TRANSMITTER);
     /* wait until ADDSEND bit is set */
     while (!i2c_flag_get(I2C0, I2C_FLAG_ADDSEND));
     /* clear ADDSEND bit */
     i2c_flag_clear(I2C0, I2C_FLAG_ADDSEND);
-    
     /* wait until the transmit data buffer is empty */
     while (!i2c_flag_get(I2C0, I2C_FLAG_TBE));
-    
     while (i)
     {
         /* data transmission */
@@ -53,45 +50,44 @@ uint8_t i2c0_write(uint8_t i2c_dev_7bit_addr, uint8_t *data, uint8_t count)
         data++;
         --i;
     }
-    
     /* send a stop condition to I2C bus */
     i2c_stop_on_bus(I2C0);
     /* wait until stop condition generate */
-    while (I2C_CTL0(I2C0)&0x0200);
-    
-    return 0; // reserved for status handling
+    while (I2C_CTL0(I2C0) & 0x0200);
+
+    return BSP_I2C_OK;
 }
 
 /*!
-\brief      write to a i2c slave 8-bit register
-\param[in]  reg: internal register
-\param[in]  value: the 8-bit value to write 
-\param[out] none
-\retval     status
+    \brief      write to a I2C slave 8-bit register
+    \param[in]  reg: internal register
+    \param[in]  value: the 8-bit value to write
+    \param[out] none
+    \retval     status
 */
-uint8_t i2c_reg8_write(uint8_t i2c_dev_7bit_addr, uint8_t reg, uint8_t value)
+i2c_state_t bsp_i2c0_reg8_write(uint8_t i2c_dev_7bit_addr, uint8_t reg, uint8_t value)
 {
     uint8_t txbuffer[2] = {reg, value};
-    
-    i2c0_write(i2c_dev_7bit_addr, (uint8_t*) txbuffer, sizeof(txbuffer));
-    
-    return 0; // reserved for status handling
+
+    bsp_i2c0_write(i2c_dev_7bit_addr, (uint8_t*) txbuffer, sizeof(txbuffer));
+
+    return BSP_I2C_OK;
 }
 
 /*!
-    \brief      read from a i2c slave 8-bit register
+    \brief      read from a I2C slave 8-bit register
     \param[in]  i2c_dev_7bit_addr: the slave's 7-bit address
-    \param[in]  data: pointer to the 8-bit variable which will hold the read value 
+    \param[in]  data: pointer to the 8-bit variable which will hold the read value
     \param[in]  count: number of bytes to read
     \param[out] none
     \retval     status
 */
-uint8_t i2c0_read(uint8_t i2c_dev_7bit_addr, uint8_t * data, uint8_t count)
+i2c_state_t bsp_i2c0_read(uint8_t i2c_dev_7bit_addr, uint8_t * data, uint8_t count)
 {
-    if (count < 1) return -1; // reserved for status handling
-    
-    int i;
-    
+    if (count < 1) return BSP_I2C_FAIL;
+
+    int i = 0;
+
     i2c_ackpos_config(I2C0, I2C_ACKPOS_NEXT);
     /* wait until I2C bus is idle */
     while (i2c_flag_get(I2C0, I2C_FLAG_I2CBSY));
@@ -99,17 +95,18 @@ uint8_t i2c0_read(uint8_t i2c_dev_7bit_addr, uint8_t * data, uint8_t count)
     i2c_start_on_bus(I2C0);
     /* wait until SBSEND bit is set */
     while (!i2c_flag_get(I2C0, I2C_FLAG_SBSEND));
-    
     /* send slave address to I2C bus */
     i2c_master_addressing(I2C0, (i2c_dev_7bit_addr << 1), I2C_RECEIVER);
     /* disable ACK before clearing ADDSEND bit */
     while (!i2c_flag_get(I2C0, I2C_FLAG_ADDSEND));
     /* clear ADDSEND bit */
     i2c_flag_clear(I2C0, I2C_FLAG_ADDSEND);
-    
-    if (count >= 3) {
-        for(i=0; i<count; i++){
-            if((count - 3) == i){
+    if (count >= 3)
+    {
+        for (i = 0; i < count; i++)
+        {
+            if ((count - 3) == i)
+            {
                 /* wait until the second last data byte is received into the shift register */
                 while (!i2c_flag_get(I2C0, I2C_FLAG_BTC));
                 /* disable acknowledge */
@@ -121,12 +118,12 @@ uint8_t i2c0_read(uint8_t i2c_dev_7bit_addr, uint8_t * data, uint8_t count)
             data[i] = i2c_data_receive(I2C0);
         }
     }
-    else if (count == 2) {
+    else if (count == 2)
+    {
         /* Wait until the last data byte is received into the shift register */
         while (!i2c_flag_get(I2C0, I2C_FLAG_BTC));
         /* wait until the RBNE bit is set */
         while (!i2c_flag_get(I2C0, I2C_FLAG_RBNE));
-        
         /* read a data from I2C_DATA */
         data[i++] = i2c_data_receive(I2C0);
         /* wait until the RBNE bit is set */
@@ -134,40 +131,38 @@ uint8_t i2c0_read(uint8_t i2c_dev_7bit_addr, uint8_t * data, uint8_t count)
         /* read a data from I2C_DATA */
         data[i] = i2c_data_receive(I2C0);
     }
-    else {  // count == 1
+    else
+    {
         /* Wait until the last data byte is received into the shift register */
         while (!i2c_flag_get(I2C0, I2C_FLAG_BTC));
         /* wait until the RBNE bit is set */
         while (!i2c_flag_get(I2C0, I2C_FLAG_RBNE));
-        
         /* read a data from I2C_DATA */
         data[i] = i2c_data_receive(I2C0);
     }
-    
     /* send a stop condition */
     i2c_stop_on_bus(I2C0);
     /* wait until stop condition generate */
-    while (I2C_CTL0(I2C0)&0x0200);
+    while (I2C_CTL0(I2C0) & 0x0200);
     i2c_ackpos_config(I2C0, I2C_ACKPOS_CURRENT);
     /* enable acknowledge */
-    i2c_ack_config(I2C0, I2C_ACK_ENABLE);  
-    
-    return 0; // reserved for status handling
+    i2c_ack_config(I2C0, I2C_ACK_ENABLE);
+
+    return BSP_I2C_OK;
 }
 
 /*!
-    \brief      read from a i2c slave 8-bit register
+    \brief      read from a I2C slave 8-bit register
     \param[in]  i2c_dev_7bit_addr: the slave's 7-bit address
     \param[in]  reg: internal register
-    \param[in]  *value: pointer to the 8-bit variable which will hold the value 
+    \param[in]  *value: pointer to the 8-bit variable which will hold the value
     \param[out] none
     \retval     status
 */
-uint8_t i2c_reg8_read(uint8_t i2c_dev_7bit_addr, uint8_t reg, uint8_t *value)
+i2c_state_t bsp_i2c0_reg8_read(uint8_t i2c_dev_7bit_addr, uint8_t reg, uint8_t *value)
 {
-    i2c0_write(i2c_dev_7bit_addr, (uint8_t*)reg, 1);
-    
-    i2c0_read(i2c_dev_7bit_addr, value, 1);
-    
-    return 0; // reserved for status handling
+    bsp_i2c0_write(i2c_dev_7bit_addr, (uint8_t*)reg, 1);
+    bsp_i2c0_read(i2c_dev_7bit_addr, value, 1);
+
+    return BSP_I2C_OK;
 }
